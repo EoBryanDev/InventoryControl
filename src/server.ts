dd17715b90f3a46a7f7b01bd76e1bd7a2b4c0dbf
EoBryanDev <mauriciobryan1998@gmail.com>
2023-04-07 14:48:22 -0300

    Build an API Documentation with Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
+import path from "path";
 import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+
 import cors from "cors";
 
-import "express-async-errors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
+
 import { router } from "./routes";
 
 const app = express();
@@ -9,7 +14,9 @@ const PORT = 3333;
 
 app.use(express.json());
 app.use(cors());
-app.use(router);
+app.use("/v1", router);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/files", express.static(path.resolve(__dirname, "..", "temp")));
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof Error) {
@@ -22,6 +29,12 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
         message: "Internal Server Error",
     });
 });
+
+app.get('/terms', (req: Request, res: Response)=> {
+    return res.json({
+        message: "Service Terms"
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT} - Controle de Estoque`);
